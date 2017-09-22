b0a1f1c510c1c7c96d2e307b32fc1c49c5b1bd56
Christina Marie Morrison <cmmorrison1@login3.cm.cluster>
2017-09-21 19:49:14 -0700

    all good

--- problems/unix-utils/cat-n.sh
+++ problems/unix-utils/cat-n.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-echo "OK"
+
+
+if [[ $# -eq 0 ]]; then
+	
+	printf "Usage: %s FILE\n" "$(basename "$0")"
+	
+	exit 1
+
+else
+	
+	if [[ -f "$1" ]]; then
+		
+		i=0
+		
+
+		while read -r LINE; do
+			let i++
+			echo $i $LINE
+	
+		done <"$1"
+	else
+		
+		printf "\"%s\" is not a file\n" "$(basename "$1")"
+		exit 1	
+	fi
+
+fi 
+
+
+
--- problems/unix-utils/head.sh
+++ problems/unix-utils/head.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-echo "OK"
+if [[ $# -eq 0 ]]; then 
+	printf "Usage: %s FILE [NUM]\n" "$(basename "$0")" 
+	exit 1 
+else	
+	if [[ -f "$1" ]]; then
+		if [[ $# -gt 1 ]]; then
+			i=0
+			while read -r LINE; do
+				let i++
+				if [[ i -le $2 ]]; then
+					echo $LINE
+				else
+					break
+				fi
+			done<"$1"
+		else 
+			i=0
+			while read -r LINE; do
+				let i++
+				if [[ i -le 3 ]]; then
+					echo $LINE
+				else
+					break
+				fi
+			done<"$1"
+		fi		
+	else
+		printf "\"%s\" is not a file\n" "$(basename "$1")"
+		exit 1
+	fi
+fi
