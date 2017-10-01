a6429880cc61cded7f7736e2a9c3ea3a8d107435
Christina Marie Morrison <cmmorrison1@login2.cm.cluster>
2017-10-01 12:24:58 -0700

    all good

--- problems/hello-py/hello.py
+++ problems/hello-py/hello.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
-"""say hello to the arguments"""
 
-print('OK')
+import sys
+import os
+
+
+def main():
+	"""main"""
+	args = sys.argv
+
+	if len(args) < 2:
+		print('Usage:', args[0], 'NAME')
+		sys.exit(1)
+
+	elif len(args) == 2:
+		print('Hello to the 1 of you: {}!' .format(args[1]))
+
+	elif len(args) == 3:
+		print('Hello to the 2 of you: {} and {}!' .format(args[1], args[2]))
+
+	else:
+		print('Hello to the {} of you: {}, and {}!' .format(len(args)-1, ', '.join(args[1:-1]), args[-1]))
+
+if __name__ == '__main__':
+	main()
--- problems/hello-py/vowel_counter.py
+++ problems/hello-py/vowel_counter.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
-"""count the vowels in a word"""
 
-print('OK')
+import sys
+import os
+
+
+def main():
+
+	"""main"""
+	args = sys.argv
+
+	if len(args) < 2:
+		print('Usage:', args[0], 'STRING')
+		sys.exit(1)
+	else:
+		word = args[1]
+		i = 0
+	
+		for letter in word:
+			if letter in 'aeiou':
+				i += 1
+
+		if i == 1:
+			print('There is {} vowel in "{}."' .format(i, word))
+
+		else:	
+			print('There are {} vowels in "{}."' .format(i, word))		
+
+if __name__ == '__main__':
+	main()
