e401773d2e65c57e712ab4681ec17b8b6ed94ed7
Christina Marie Morrison <cmmorrison1@login2.cm.cluster>
2017-10-19 20:24:03 -0700

    all good

--- problems/proteins/prot.py
+++ problems/proteins/prot.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env python3
 
-print('OK')
+import sys
+import os
+
+input = sys.argv
+if len(input) !=2:
+	print('Usage: {} SEQ'.format(sys.argv[0]))
+	sys.exit(1)
+rna = input[1]
+
+procode = dict()
+file = open('codons.rna').read().split()
+
+count = 0
+for line in file:
+	if count % 2 ==0:
+		procode[line] = '' 	
+		prev = line
+	else:
+		procode[prev] = line
+	count+=1
+k=3
+rnapiece = []
+for i in range(0, len(rna), k):
+	rnapiece.append(rna[i:i+k])
+
+protein = []
+for line in rnapiece:
+	for key in procode.keys():
+		if line.lower() == key.lower():
+			protein.append(procode[key])
+
+print('{}'.format(''.join(protein)))
+	
+
+
