07a07cc7efb3b87a18dc609380be8d794c6f73a6
Christina Marie Morrison <cmmorrison1@login2.cm.cluster>
2017-11-13 20:38:11 -0700

    all good

--- problems/parse-blast/parse_blast_tab.py
+++ problems/parse-blast/parse_blast_tab.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python3
 
-print('OK')
+import argparse
+import csv
+import os
+import sys
+
+# --------------------------------------------------
+def get_args():
+    parser = argparse.ArgumentParser(description='Parse Blast Tab')
+    parser.add_argument('tab', metavar='tab', help='Blast Tabulated Output')
+    parser.add_argument('-p', '--pct_id', help='Percent Identity',
+                        metavar='float', type=float, default=0.0)
+    parser.add_argument('-e', '--evalue', help='maximum evalue',
+                        metavar='float', type=float, default=1.0)
+    return parser.parse_args()
+
+# --------------------------------------------------
+def main():
+    args = get_args()
+    blastfile = args.tab
+    pctid = args.pct_id
+    evalue = args.evalue
+    flds = 'qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore'.split()
+
+    for line in open(blastfile):
+        row = dict(zip(flds, line.split('\t')))
+        if float(row['pident']) >= pctid and float(row['evalue']) <= evalue:
+            print('{}\t{}\t{}\t{}'.format(row['qseqid'], row['sseqid'], row['pident'], row['evalue']))   
+    
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
